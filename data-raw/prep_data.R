e41bc9094154f83e967e2c01c23fffaa57841b3c
ColinFay <contact@colinfay.me>
2017-10-07 23:02:25 +0200

    doc for corpus

--- data-raw/prep_data.R
+++ data-raw/prep_data.R
@@ -2,4 +2,5 @@
 
 library(readr)
 corpus <- read_delim(file = "data-raw/Lexique381.txt", delim = "\t")
-use_data(corpus)
+names(corpus) <- gsub('[0-9]*_', '', names(corpus))
+devtools::use_data(corpus, overwrite = TRUE)
